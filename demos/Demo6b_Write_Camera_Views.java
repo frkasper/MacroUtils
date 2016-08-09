8aa0dbb7147b4d60a076f8cfee4360a04d14b78e
Fabio Kasper <frkasper@ibest.com.br>
2016-08-09 14:09:13 -0300

    Added Demo6_Scene_Resolution.java and extensions.

---
+++ demos/Demo6b_Write_Camera_Views.java
@@ -0,0 +1,10 @@
+package macroutils;
+
+public class Demo6b_Write_Camera_Views extends MacroUtils {
+
+  public void execute() {
+    _initUtils();
+    writeCameraViews("myCameras.txt");
+  }
+  
+}
