760817b1613e825bce27c76b8efe6e55ec56a6cd
Fabio Kasper <frkasper@ibest.com.br>
2016-11-10 10:01:20 -0200

    Added SimpleHexaMesher Simulation Assistant.

---
+++ simassistants/simplehexamesher/BlockTask.java
@@ -0,0 +1,126 @@
+package simplehexamesher;
+
+import macroutils.*;
+import star.assistant.*;
+import star.assistant.annotation.*;
+import star.assistant.ui.*;
+import star.base.neo.*;
+import star.common.*;
+import star.vis.*;
+
+/**
+ * Block Mesher Task.
+ *
+ * @since MacroUtils v11.06.
+ * @author Fabio Kasper
+ */
+@StarAssistantTask(
+        display = "Block Mesher",
+        contentPath = "html/BlockTask.xhtml",
+        controller = BlockTask.MesherController.class
+)
+public class BlockTask extends Task {
+
+    public BlockTask(MacroUtils m) {
+        _mu = m;
+    }
+
+    public class MesherController extends FunctionTaskController {
+
+        /**
+         * Generate Mesh Task.
+         */
+        public void generateMesh() {
+            initializeTask();
+            if (!haveParameters()) {
+                return;
+            }
+            Annotation an = _mu.get.objects.annotation(ANNOT_NAME, false);
+            boolean is2D = an != null;
+            _mu.templates.mesh.setBadgeFor2D(is2D);
+            VectorGlobalParameter c1 = (VectorGlobalParameter) _mu.get.objects.parameter(BLOCK_C1, false);
+            VectorGlobalParameter c2 = (VectorGlobalParameter) _mu.get.objects.parameter(BLOCK_C2, false);
+            VectorGlobalParameter nc = (VectorGlobalParameter) _mu.get.objects.parameter(BLOCK_NCELLS, false);
+            Region r = _mu.templates.mesh.hexaBlock(getDouble(c1), getDouble(c2), c1.getQuantity().getUnits(),
+                    getInt(nc), "Block");
+            if (is2D) {
+                _ud.namedObjects.add(r);
+                an.getAnnotationManager().remove(an);
+                _mu.templates.mesh.setBadgeFor2D(false);
+            } else {
+                _ud.namedObjects.addAll(_mu.get.boundaries.all(r, false));
+            }
+            Scene scn = _mu.add.scene.mesh(_ud.namedObjects);
+            scn.openScene();
+            if (!is2D) {
+                scn.setViewOrientation(new DoubleVector(new double[]{-1.0, 1.0, -1.0}),
+                        new DoubleVector(new double[]{0.0, 1.0, 0.0}));
+            }
+            scn.resetCamera();
+            _ud.namedObjects.clear();
+            _mu.io.say.action("Generate Mesh Task is finished.", true);
+        }
+
+        /**
+         * Set as Two-Dimensional Task.
+         */
+        public void setAs2D() {
+            initializeTask();
+            _mu.add.tools.annotation_Simple(ANNOT_NAME, "");
+            _mu.io.say.action("Geometry will be flagged as 2D.", true);
+            _mu.io.say.action("Set as Two-Dimensional Task is finished.", true);
+        }
+
+        /**
+         * Set Parameters Task.
+         */
+        public void setParameters() {
+            initializeTask();
+            VectorGlobalParameter vgp = _mu.add.tools.parameter_Vector(BLOCK_C1, new double[]{0, 0, 0}, _ud.unit_m);
+            _mu.add.tools.parameter_Vector(BLOCK_C2, new double[]{1, 1, 1}, _ud.unit_m);
+            _mu.add.tools.parameter_Vector(BLOCK_NCELLS, new double[]{2, 4, 6}, _ud.unit_Dimensionless);
+            selectNodeExclusive(vgp);
+            _mu.io.say.action("Set Parameters Task is finished.", true);
+        }
+
+        private double[] getDouble(VectorGlobalParameter vgp) {
+            return vgp.getQuantity().getVector().toDoubleArray();
+        }
+
+        private int[] getInt(VectorGlobalParameter vgp) {
+            double[] doubleArray = getDouble(vgp);
+            int[] intArray = new int[doubleArray.length];
+            for (int i = 0; i < doubleArray.length; ++i) {
+                intArray[i] = (int) doubleArray[i];
+            }
+            return intArray;
+        }
+
+        private boolean haveParameters() {
+            if (_mu.get.objects.parameter(BLOCK_C1, false) == null) {
+                _mu.io.say.loud("Please define Block dimensions first and try again.");
+                return false;
+            }
+            return true;
+        }
+
+        private void initializeTask() {
+            _sim = getActiveSimulation();
+            _mu.setSimulation(_sim, false);
+            _ud = _mu.userDeclarations;
+        }
+
+    }
+
+    //--
+    //-- Variables declaration area.
+    //--
+    private final MacroUtils _mu;
+    private Simulation _sim;
+    private UserDeclarations _ud;
+    private static final String ANNOT_NAME = "is2D";
+    private static final String BLOCK_C1 = "Block Coordinate1";
+    private static final String BLOCK_C2 = "Block Coordinate2";
+    private static final String BLOCK_NCELLS = "Block Number of Cells";
+
+}
---
+++ simassistants/simplehexamesher/IntroductionTask.java
@@ -0,0 +1,13 @@
+package simplehexamesher;
+
+import star.assistant.*;
+import star.assistant.annotation.*;
+
+/**
+ * Introduction Task.
+ *
+ * @since MacroUtils v11.06.
+ * @author Fabio Kasper
+ */
+@StarAssistantTask(display = "Introduction", contentPath = "html/IntroductionTask.xhtml")
+public class IntroductionTask extends Task { }
