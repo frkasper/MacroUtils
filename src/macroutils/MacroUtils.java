0a50b7f9e56347852a087263e5ab52872a9771d9
Fabio Kasper <fabio.kasper@cd-adapco.com>
2018-04-24 16:01:17 -0400

    Opening branch; initial commit

--- src/macroutils/MacroUtils.java
+++ src/macroutils/MacroUtils.java
@@ -26,16 +26,16 @@ import star.common.SimulationIterator;
  * <p>
  * <b>Requires:</b>
  * <ul>
- * <li> STAR-CCM+ v13.02 libraries. <u>It may not run in other versions</u>;
+ * <li> STAR-CCM+ v13.04 libraries. <u>It may not run in other versions</u>;
  * </ul>
  *
  * @since STAR-CCM+ v7.02, May of 2012
  * @author Fabio Kasper
- * @version v13.02, March 02, 2018.
+ * @version v13.04, April 24, 2018.
  */
 public final class MacroUtils {
 
-    private final String MACROUTILS_VERSION = "MacroUtils version 13.02 (build 1)";
+    private final String MACROUTILS_VERSION = "MacroUtils version 13.04 (build 1)";
 
     /**
      * Initialize MacroUtils in intrusive mode by providing a Simulation object.
